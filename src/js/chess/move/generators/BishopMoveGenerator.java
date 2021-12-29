b60ae3a3e82f1fb85caf19d3997e2059d81f72c0
nalf3in <nalf3in@gmail.com>
2021-12-28 20:51:55 -0500

    Fixes to the bishop move generator

--- src/js/chess/move/generators/BishopMoveGenerator.java
+++ src/js/chess/move/generators/BishopMoveGenerator.java
@@ -1,34 +1,57 @@
 package js.chess.move.generators;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import js.chess.Board;
 import js.chess.Color;
+import js.chess.Square;
 import js.chess.move.Move;
 import js.chess.move.MoveGenerator;
+import js.chess.move.MoveType;
 
-public class BishopMoveGenerator implements MoveGenerator{
-    
+public class BishopMoveGenerator implements MoveGenerator {
+
     private Board board;
 
-    private int[] offsets = {
-        -9, -7, 7, 9,
-        -10, -8, 8, 10,
-        -11, -9, 9, 11,
-        -12, -10, 10, 12,
-        -13, -11, 11, 13,
-        -14, -12, 12, 14,
-        -15 , -13, 13, 15
+    private final int[][] offsets = {
+            { -9, -18, -27, -36, -45, -54, -63 }, // top left diagonal
+            { -7, -14, -21, -28, -35, -42, -49 }, // top right diagonal
+            { 7, 14, 21, 28, 35, 42, 49 }, // bottom left diagonal
+            { 9, 18, 27, 36, 45, 54, 63 }, // bottom right diagonal
     };
-    
+
     public BishopMoveGenerator(Board board) {
         this.board = board;
     }
 
     @Override
     public List<Move> getPossibleMoves(int currentPos, Color color) {
-        // TODO Auto-generated method stub
-        return null;
+        List<Move> toReturn = new ArrayList<>(offsets.length);
+
+        for (int[] line : offsets) {
+            int previousPos = currentPos;
+
+            for (int offset : line) {
+                int nextPos = currentPos + offset;
+                if (nextPos >= 0 && nextPos < 64) {
+
+                    if (Math.abs((nextPos / 8) - (previousPos / 8)) != 1)
+                        break; // We skip to the next diagonal if we are "out of bounds"
+
+                    Square square = board.getSquares().get(nextPos);
+
+                    if (square.isEmpty()) {
+                        toReturn.add(new Move(currentPos, nextPos, MoveType.QUIET_MOVE));
+                    } else if (square.getPiece().get().getColor() != color) {
+                        toReturn.add(new Move(currentPos, nextPos, MoveType.CAPTURES));
+                        break; // We stop once we meet the first piece in the line
+                    }
+
+                    previousPos = nextPos;
+                }
+            }
+        }
+        return toReturn;
     }
-    
 }
--- tests/TestBishopMoveGenerator.java
+++ tests/TestBishopMoveGenerator.java
@@ -9,21 +9,6 @@ import js.chess.move.generators.BishopMoveGenerator;
 
 public class TestBishopMoveGenerator {
 
-    private static int[] offsets1 = {
-            -9, -18, -27, -36, -45, -54, -63, // top left diagonal
-            -7, -14, -21, -28, -35, -42, -49, // top right diagonal
-            7, 14, 21, 28, 35, 42, 49, // top left diagonal
-            9, 18, 27, 36, 45, 54, 63 // bottom right diagonal
-    };
-
-    // Could also do it in 1d array but that's for another day
-    private static int[][] offsets = {
-            {-9, -18, -27, -36, -45, -54, -63}, // top left diagonal
-            {-7, -14, -21, -28, -35, -42, -49}, // top right diagonal
-            {7, 14, 21, 28, 35, 42, 49},        // bottom left diagonal 
-            {9, 18, 27, 36, 45, 54, 63},        // bottom right diagonal
-    };
-
     public static void main(String[] args) {
 
         Board board = new Board();
@@ -32,31 +17,18 @@ public class TestBishopMoveGenerator {
         Piece whitePiece1 = new Piece("♗", Color.WHITE);
         Piece test = new Piece("X", Color.BLACK);
 
-        int pos = 23;
-
+        int pos = 63;
         squares.get(pos).setPiece(whitePiece1);
-
-        for (int[] line : offsets) {
-            int previousPos = pos;
-
-            for (int offset : line) {
-                int nextPos = pos + offset;
-                if (nextPos >= 0 && nextPos < 64) {
-
-                    if (Math.abs((nextPos / 8) - (previousPos / 8)) != 1)
-                        break; // We skip to the next diagonal if we are "out of bounds"
-
-                    squares.get(nextPos).setPiece(test);
-                    previousPos = nextPos;
-                }
-            }
-        }
+        squares.get(54).setPiece(whitePiece1);
 
         BishopMoveGenerator bishopMoveGenerator = new BishopMoveGenerator(board);
-        System.out.println(board.toString());
+        List<Move> possibleMoves = bishopMoveGenerator.getPossibleMoves(pos, Color.BLACK);
 
-        List<Move> possibleMoves = bishopMoveGenerator.getPossibleMoves(35, Color.BLACK);
+        for (Move move : possibleMoves) { 
+            squares.get(move.getTo()).setPiece(test);
+        }
 
         System.out.println(board.toString());
+        System.out.println("damn");
     }
 }
